e5e4dd0a185a1b27f89d74a226b0792c6d516a5c
MickMake <github@mickmake.com>
2020-03-17 14:41:57 +1100

    Older releases.

---
+++ 1.3.0/build/unit-tests/terminus/02-release.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Created on 2020-03-17T14:41:32+1100, using template:02-release.sh.tmpl and json:gearbox.json
+
+p_info "terminus-1.3.0" "Release test started."
+
+ps -eaf
+
+p_info "terminus-1.3.0" "Release test finished."
